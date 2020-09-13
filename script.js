8e14203b54f1a33adc4cd2ef603039f4370befc4
Gustavo Souza <GustavoSouzaAlves12@gmail.com>
2020-09-13 00:18:08 -0300

    create snake function added

--- script.js
+++ script.js
@@ -1,10 +1,23 @@
 const canvas = document.querySelector("#snake")
 let context = canvas.getContext("2d")
-let box = 32;
+let box = 32
 
-function criarBG(){
+let snake = [] 
+snake[0] = {
+    x:8 * box,
+    y:8 * box
+}
+function createBG(){
     context.fillStyle = "blue"
     context.fillRect(0,0,16*box, 16*box)
 }
 
-criarBG()
+function createSnake(){
+    for(i=0;i < snake.length; i++){
+        context.fillStyle = "red"
+        context.fillRect(snake[i].x,snake[i].y, box, box)
+    }
+}
+
+createBG()
+createSnake()
