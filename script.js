26ff3af426cec3e1cc910a9f43bb0cda14de57d8
Gustavo Souza <GustavoSouzaAlves12@gmail.com>
2020-09-13 00:05:51 -0300

    first commit

---
+++ script.js
@@ -0,0 +1,10 @@
+const canvas = document.querySelector("#snake")
+let context = canvas.getContext("2d")
+let box = 32;
+
+function criarBG(){
+    context.fillStyle = "blue"
+    context.fillRect(0,0,16*box, 16*box)
+}
+
+criarBG()
